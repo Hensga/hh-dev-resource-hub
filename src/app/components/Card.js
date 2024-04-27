4ffe359296cdc9a01adaeb5f045d20809ddec3ac
Henzibit <56224913+Henzibit@users.noreply.github.com>
2024-04-27 17:14:40 +0200

    changed config

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -2,25 +2,6 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 
-function ArrowIcon(props) {
-  return (
-    <svg
-      className={props.className}
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        fill="#181818"
-        fillRule="evenodd"
-        d="M8.25 3.75H19.5a.75.75 0 0 1 .75.75v11.25a.75.75 0 0 1-1.5 0V6.31L5.03 20.03a.75.75 0 0 1-1.06-1.06L17.69 5.25H8.25a.75.75 0 0 1 0-1.5"
-        clipRule="evenodd"
-      />
-    </svg>
-  );
-}
-
 const textVariants = {
   hover: {
     y: -30,
