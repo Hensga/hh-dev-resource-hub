240e47bab689d17a50c8d4ff8547ce9a21ff907f
Henzibit <56224913+Henzibit@users.noreply.github.com>
2024-04-27 17:20:12 +0200

    removed gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/hh-dev-resource-hub",
   images: {
     unoptimized: true,
     remotePatterns: [
